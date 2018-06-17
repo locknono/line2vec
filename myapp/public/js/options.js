e450e4ff9b12498d0454b247511584a86aa1be5f
locknono <guozy0103@outlook.com>
2018-06-17 15:18:19 +0800

    Add label color scale to options.js

--- myapp/public/js/options.js
+++ myapp/public/js/options.js
@@ -1,22 +1,41 @@
 var op = (function () {
+    //file
     var system_name = "Wenzhou";
     var root_path = 'data/BS/';
     var res_path = root_path + 'res/';
-    var img_root_path='./images/source/'
+    var img_root_path = './images/source/'
     var fileList = {
         id_location_file: root_path + 'stationIDLocation.csv',
         pixel_file: root_path + 'pixel.json',
         track_file: root_path + '18Data_track.json',
         original_file: root_path + 'linedetail_label.csv',
-        sample_file : root_path + 'linedetail_label.csv'
+        sample_file: root_path + 'linedetail_label.csv'
     };
     var pixel_file = root_path + 'pixel.json';
     var track_file = root_path + '18Data_track.json';
-    var originalScatterImg=img_root_path+'original.png';
+    var originalScatterImg = img_root_path + 'original.png';
+
+    var labelColorScale = d3
+        .scaleOrdinal()
+        .domain([-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+        .range([
+            "#fb8072",
+            "#80b1d3",
+            "#fdb462",
+            "#b3de69",
+            "#fccde5",
+            "#d9d9d9",
+            "#bc80bd",
+            "#ccebc5",
+            "#ffed6f",
+            "#8dd3c7",
+            "#ffffb3",
+            "#CCCCCC"
+        ]);
     //style
     var minArtLineWidth = 2,
         maxArtLineWidth = 15,
-    circleBarStartColor = "#fff4b3",
+        circleBarStartColor = "#fff4b3",
         circleBarEndColor = "#F22613",
         scatterColor = 0xd9d9d9;
     return {
@@ -31,6 +50,7 @@ var op = (function () {
         scatterColor: scatterColor,
         res_path: res_path,
         pixel_file: pixel_file,
-        originalScatterImg:originalScatterImg,
+        originalScatterImg: originalScatterImg,
+        labelColorScale:labelColorScale,
     }
 }())
