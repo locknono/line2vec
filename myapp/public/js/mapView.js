0cffffce0471c57cca105194de9fe2e7bed74a39
locknono <guozy0103@outlook.com>
2018-06-19 19:32:28 +0800

    Drawing Art line with color gradient

--- myapp/public/js/mapView.js
+++ myapp/public/js/mapView.js
@@ -30,7 +30,6 @@ var options = {
   removalMode: true // adds a button to remove layers
 };
 map.pm.addControls(options);
-
 var selectTime = 0;
 var clickSourceLatLng;
 var clickProjectSourceLatLng;
@@ -364,7 +363,7 @@ function load(
           function (selection, projection) {
             map.off("pm:create");
 
-            function drawArtLine(timeString,random=false) {
+            function drawArtLine(timeString, random = false) {
               new Promise(function (resolve, reject) {
                 $.ajax({
                   type: "post",
@@ -373,7 +372,7 @@ function load(
                     selectedMapData: selectedMapData,
                     timeString: timeString,
                     sFile: op.getSampleFile(),
-                    randomFlag:random,
+                    randomFlag: random,
                   },
                   success: function (data) {
                     resolve(data);
@@ -416,24 +415,6 @@ function load(
                     return b.lineCoors.length - a.lineCoors.length;
                   });
                   selection.selectAll(".artLine").remove();
-                  var colorDefs = selection.append("defs");
-                  var linearGradient = colorDefs
-                    .append("linearGradient")
-                    .attr("id", "linearColor")
-                    .attr("x1", "0%")
-                    .attr("y1", "0%")
-                    .attr("x2", "100%")
-                    .attr("y2", "100%");
-                  var stop1 = linearGradient
-                    .append("stop")
-                    .attr("offset", "0%")
-                    .style("stop-color", d3.interpolateYlGnBu(0).toString());
-
-                  var stop2 = linearGradient
-                    .append("stop")
-                    .attr("offset", "100%")
-                    .style("stop-color", d3.interpolateYlGnBu(1).toString());
-
                   var lineGenarator = d3
                     .line()
                     .x(function (d) {
@@ -442,7 +423,7 @@ function load(
                     .y(function (d) {
                       return projection.latLngToLayerPoint(d).y;
                     })
-                    .curve(d3.curveNatural);
+                    .curve(d3.curveCatmullRomOpen);
 
                   var defs = selection.append("defs");
                   var arrowMarker = defs
@@ -530,17 +511,136 @@ function load(
                     } else if (coorsSet.size > 2) {
                       var path = lineGenarator(allTrack[i].lineCoors);
                     }
+
+                    var colorDefs = selection.append("defs");
+                    var linearGradient = colorDefs
+                      .append("linearGradient")
+                      .attr("id", "linearColor")
+                      .attr("x1", allTrack[0][0])
+                      .attr("y1", allTrack[0][1])
+                      .attr("x2", allTrack[1][0])
+                      .attr("y2", allTrack[1][1]);
+                    linearGradient
+                      .append("stop")
+                      .attr("offset", "0%")
+                      .style("stop-color", d3.interpolateYlGnBu(0).toString());
+                    linearGradient
+                      .append("stop")
+                      .attr("offset", "100%")
+                      .style("stop-color", d3.interpolateYlGnBu(1).toString());
+
                     selection
                       .append("path")
                       .attr("class", "artLine")
-                      .style(
-                        "stroke",
-                        "url(#" + linearGradient.attr("id") + ")"
-                      )
+                      .attr("id", function () {
+                        return "artLine" + i;
+                      })
+                      .style("stroke", function () {
+                        return "url(#" + linearGradient.attr("id") + ")"
+                      })
                       .attr("stroke-width", widthScale(Math.log2(allTrack[i].value)))
                       .attr("fill", "none")
                       .style("stroke-linecap", "round")
                       .attr("d", path);
+
+                    var color = d3.interpolate(d3.interpolateYlGnBu(0), d3.interpolateYlGnBu(1));
+                    var path = selection.selectAll("[id='artLine" + i + "']").remove();
+                    if (coors.length > 2 && coorsSet.size > 2) {
+                      path
+                        .data(quads(samples(path.node(), 2)))
+                        .enter().append("path")
+                        .style("fill", function (d) {
+                          return color(d.t);
+                        })
+                        .style("stroke", function (d) {
+                          return color(d.t);
+                        })
+                        .attr("d", function (d) {
+                          for (var s = 0; s < d.length; s++) {
+                            console.log(d);
+                            if (d[s] === undefined || Number.isNaN(d[s])) {
+                              console.log(d);
+                              console.log('path.node(): ', path.node());
+                              break;
+                            }
+                          }
+                          return lineJoin(d[0], d[1], d[2], d[3], widthScale(Math.log2(allTrack[i].value)));
+                        });
+
+                    }
+
+
+                    // Sample the SVG path uniformly with the specified precision.
+                    function samples(path, precision) {
+                      var n = path.getTotalLength(),
+                        t = [0],
+                        i = 0,
+                        dt = precision;
+                      while ((i += dt) < n) t.push(i);
+                      t.push(n);
+                      return t.map(function (t) {
+                        var p = path.getPointAtLength(t),
+                          a = [p.x, p.y];
+                        a.t = t / n;
+                        return a;
+                      });
+                    }
+
+                    // Compute quads of adjacent points [p0, p1, p2, p3].
+                    function quads(points) {
+                      return d3.range(points.length - 1).map(function (i) {
+                        var a = [points[i - 1], points[i], points[i + 1], points[i + 2]];
+                        a.t = (points[i].t + points[i + 1].t) / 2;
+                        return a;
+                      });
+                    }
+
+                    // Compute stroke outline for segment p12.
+                    function lineJoin(p0, p1, p2, p3, width) {
+                      var u12 = perp(p1, p2),
+                        r = width / 2,
+                        a = [p1[0] + u12[0] * r, p1[1] + u12[1] * r],
+                        b = [p2[0] + u12[0] * r, p2[1] + u12[1] * r],
+                        c = [p2[0] - u12[0] * r, p2[1] - u12[1] * r],
+                        d = [p1[0] - u12[0] * r, p1[1] - u12[1] * r];
+
+                      if (p0) { // clip ad and dc using average of u01 and u12
+                        var u01 = perp(p0, p1),
+                          e = [p1[0] + u01[0] + u12[0], p1[1] + u01[1] + u12[1]];
+                        a = lineIntersect(p1, e, a, b);
+                        d = lineIntersect(p1, e, d, c);
+                      }
+
+                      if (p3) { // clip ab and dc using average of u12 and u23
+                        var u23 = perp(p2, p3),
+                          e = [p2[0] + u23[0] + u12[0], p2[1] + u23[1] + u12[1]];
+                        b = lineIntersect(p2, e, a, b);
+                        c = lineIntersect(p2, e, d, c);
+                      }
+                      return "M" + a + "L" + b + " " + c + " " + d + "Z";
+                    }
+
+                    // Compute intersection of two infinite lines ab and cd.
+                    function lineIntersect(a, b, c, d) {
+                      var x1 = c[0],
+                        x3 = a[0],
+                        x21 = d[0] - x1,
+                        x43 = b[0] - x3,
+                        y1 = c[1],
+                        y3 = a[1],
+                        y21 = d[1] - y1,
+                        y43 = b[1] - y3,
+                        ua = (x43 * (y1 - y3) - y43 * (x1 - x3)) / (y43 * x21 - x43 * y21);
+                      return [x1 + ua * x21, y1 + ua * y21];
+                    }
+
+                    // Compute unit vector perpendicular to p01.
+                    function perp(p0, p1) {
+                      var u01x = p0[1] - p1[1],
+                        u01y = p1[0] - p0[0],
+                        u01d = Math.sqrt(u01x * u01x + u01y * u01y);
+                      return [u01x / u01d, u01y / u01d];
+                    }
 
                     /* if (
                       allTrack[i].value >= flowSliderValue
@@ -1103,7 +1203,7 @@ function load(
               $("#randomSample").on("click", function () {
                 if ($("#randomSample").is(":checked")) {
                   if (selectedMapData.length !== 0) {
-                    drawArtLine(timeString,random=true);
+                    drawArtLine(timeString, random = true);
                   }
                 }
               });
